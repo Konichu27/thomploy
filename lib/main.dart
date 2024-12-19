cc7eacfcb3922069032512b830f2d139826f9230
Konichu27 <neodayao@gmail.com>
2024-12-20 03:01:32 +0800

    Adds a placeholder icon, as well as untested log-in functionality. Pages have been drafted (thanks Rayna!) but not yet implemented, this will come later after testing Firebase functionality.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,93 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'app_state.dart';
+import 'home_page.dart';
+import 'login_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Provider class is responsible for managing the state of the application
+  runApp(ChangeNotifierProvider(
+    create: (context) => ApplicationState(),
+    builder: ((context, child) => const ThomployApp()),
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class ThomployApp extends StatelessWidget {
+  const ThomployApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'Thomploy',
+      theme: ThemeData(
+        primaryColor: const Color(0xFFFFD700), // Bright Yellow
+        scaffoldBackgroundColor: const Color(0xFFFFD700),
       ),
+      routerConfig: _router, // new
     );
+
   }
 }
+
+// GoRouter for routing pages in the app,
+// particularly Firebase-related ones
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomePage(),
+      routes: [
+        GoRoute(
+          path: 'log-in',
+          builder: (context, state) {
+            return SignInScreen(
+              actions: [
+                AuthStateChangeAction(((context, state) {
+                  final user = switch (state) {
+                    SignedIn state => state.user,
+                    UserCreated state => state.credential.user,
+                    _ => null
+                  };
+                  if (user == null) {
+                    return;
+                  }
+                  if (state is UserCreated) {
+                    user.updateDisplayName(user.email!.split('@')[0]);
+                  }
+                  if (!user.emailVerified) {
+                    user.sendEmailVerification();
+                    const snackBar = SnackBar(
+                        content: Text(
+                            'Please check your email to verify your email address'));
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+                  context.pushReplacement('/');
+                })),
+              ],
+            );
+          },
+        ),
+        GoRoute(
+          path: 'profile',
+          builder: (context, state) {
+            return ProfileScreen(
+              providers: const [],
+              actions: [
+                SignedOutAction((context) {
+                  context.pushReplacement('/');
+                }),
+              ],
+            );
+          },
+        ),
+      ],
+    ),
+  ],
+);
